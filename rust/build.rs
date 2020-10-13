ce2dd05224176f5d0e27b93b20985471b02b6105
Michael Angelo Calimlim <macalimlim@gmail.com>
2020-10-14 00:35:41 +0800

    separate godot and rust project directories

---
+++ rust/build.rs
@@ -0,0 +1,9 @@
+use gdnative_project_utils::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let classes = scan_crate("src")?;
+    Generator::new()
+        .godot_project_dir("../godot")
+        .build(classes)?;
+    Ok(())
+}
