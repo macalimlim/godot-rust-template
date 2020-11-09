f525a917f31fbc9ab1c535fe95fd87026f35a35f
Michael Angelo Calimlim <macalimlim@gmail.com>
2020-11-09 21:50:25 +0800

    disable build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,9 +1,11 @@
-use gdnative_project_utils::*;
+//use gdnative_project_utils::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /*
     let classes = scan_crate("src")?;
     Generator::new()
         .godot_project_dir("../godot")
         .build(classes)?;
+    */
     Ok(())
 }
